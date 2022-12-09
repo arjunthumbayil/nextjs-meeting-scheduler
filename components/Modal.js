cb4a7b4c80aba300e59a55bb8420f06fbf7e1f95
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-12-09 05:51:14 +0530

    introduced clearTimeout inside <Modal />

--- components/Modal.js
+++ components/Modal.js
@@ -3,9 +3,11 @@ import styles from "../styles/Home.module.css";
 
 const Modal = ({ modalContent, closeModal }) => {
   useEffect(() => {
-    setTimeout(() => {
+    const timeout = setTimeout(() => {
       closeModal();
     }, 4500);
+
+    return () => clearTimeout(timeout);
   });
   return (
     <div className={styles.modal}>
